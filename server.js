b79ff041386e735d45f6eb6faa4cb6e5ed46cd0a
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-02-08 04:50:06 +0000

    Rendering index page without styles

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const app = express();
 
 
 // PORT 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+// ROUTES 
+const index_router = require('./router/index_router');
+
+
+app.use(index_router);
 
 
 app.listen(PORT, ()=>{
