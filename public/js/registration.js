4d2cd0bda35ed1a1be07fda892c1a05d7f027963
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-02-18 19:29:01 +0000

    Collect registration data from frontend

---
+++ public/js/registration.js
@@ -0,0 +1,133 @@
+
+let registration_form = document.querySelector('#registration-form');
+
+// Personal Details fields 
+let first_name = document.getElementById('first-name');
+let middle_name = document.getElementById('middle-name');
+let last_name = document.getElementById('last-name');
+let date_of_birth = document.getElementById('date-of-birth');
+let home_address = document.getElementById('home-address');
+let contact_number = document.getElementById('contact-number');
+let NIN_number = document.getElementById('nin-number');
+let national_id = document.getElementById('national-id-card');
+let position_at_job = document.getElementById('position');
+let salary_deposit_to_mobile_money = document.getElementById('salary-acceptance');
+let mobile_money_number = document.getElementById('mobile-money-number');
+let monthly_salary = document.getElementById('salary-per-month');
+let currency = document.getElementById('currency');
+// console.log(first_name, middle_name, last_name, date_of_birth, home_address, contact_number, position, salary_payment_means, mobile_money_number, salary_per_month, currency);
+
+
+//Employer Details field
+let employer_name = document.getElementById('employer-name');
+let employer_address = document.getElementById('employer-address');
+let hr_fullname = document.getElementById('hr-full-name');
+let hr_home_address = document.getElementById('hr-home-address');
+let hr_phone_number = document.getElementById('hr-contact-number');
+let employment_letter = document.getElementById('employment-letter');
+let employment_contract = document.getElementById('employment-contract');
+console.log(employment_contract, employment_letter);
+// console.log(employer_name, employer_address, hr_full_name, hr_home_address, hr_contact_number);
+
+
+// Gurantor Details fields 
+let gurantor_fullname = document.getElementById('gurantor-fullname');
+let gurantor_date_of_birth = document.getElementById('gurantor-dob');
+let gurantor_home_address = document.getElementById('gurantor-home-address');
+let gurantor_phone_number = document.getElementById('gurantor-phone-number');
+let gurantor_relationship_to_creditor = document.getElementById('gurantor-relationship');
+let mou_from_guarantor = document.getElementById('guarantor-mou');
+let guarantor_government_issued_id = document.getElementById('guarantor-id-card');
+let policy = document.getElementById('policy');
+console.log(mou_from_guarantor, guarantor_government_issued_id);
+// console.log(gurantor_fullname, gurantor_dob, gurantor_home_address, gurantor_phone_number, gurantor_relationship, policy)
+
+
+//Will save extracted data here
+let data = {};
+
+
+registration_form.addEventListener('submit', getFrontEndData);
+
+
+
+
+
+// extract data from the form and sanitize it 
+function getFrontEndData (event){
+
+    // prevent form default behaviour
+    event.preventDefault();
+
+
+    // extract values from field and add it to data object 
+    data.first_name = first_name.value;
+    data.middle_name = middle_name.value;
+    data.last_name = last_name.value;
+    data.date_of_birth = date_of_birth.value;
+    data.home_address = home_address.value;
+    data.contact_number = contact_number.value;
+    data.NIN_number = NIN_number.value;
+    data.national_id = national_id.value;
+    data.position_at_job = position_at_job.value
+    data.salary_deposit_to_mobile_money = salary_deposit_to_mobile_money.value;
+    data.mobile_money_number = mobile_money_number.value;
+    data.monthly_salary = monthly_salary.value;
+    data.currency = currency.value;
+    // employer's info 
+    data.employer_name = employer_name.value;
+    data.employer_address = employer_address.value;
+    data.hr_fullname = hr_fullname.value;
+    data.hr_home_address = hr_home_address.value;
+    data.hr_phone_number = hr_phone_number.value;
+    data.employment_letter = employment_letter.value;
+    data.employment_contract = employment_contract.value;
+    //gurantor info
+    data.gurantor_fullname = gurantor_fullname.value;
+    data.gurantor_date_of_birth = gurantor_date_of_birth.value
+    data.gurantor_home_address = gurantor_home_address.value;
+    data.gurantor_phone_number = gurantor_phone_number.value;
+    data.gurantor_relationship_to_creditor = gurantor_relationship_to_creditor.value;
+    // data.policy = policy.value;
+
+    console.log(data);
+
+    let url = '/register';
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(data)
+    })
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+    return data;
+
+};
+
+// function getSeletedValueFromDropDown(){
+
+// }
+
+
+//send data to backend api
+// function submitFronEndData(data){
+
+// }
+
+// let url = '/register';
+
+// fetch(url, {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//         // 'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//     body: JSON.stringify(data)
+// })
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
