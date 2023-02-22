ccf2481b1243b8eded3c1d95f4517fec318b8e7c
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-02-22 17:15:28 +0000

    Add JWT for authentication

--- router/login_router.js
+++ router/login_router.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+// const jwt = require('jsonwebtoken');
+const jwtSecret = '9c1bcf23c2cd0fb8e0563fdd63343ec4220750129ae617d703383d6cfcf60f1138d37c';
+
 
 
 router.get('/login', (req,res)=>{
@@ -28,6 +31,32 @@ router.post('/login', (req, res)=>{
           result = response.data;
 
           res.send(result);
+
+
+          //new code
+
+           //configure and create a sign token
+          //  console.log('configuring and creating a sign token');
+          //  const maxAge = 3 * 60 * 60; //token life span 
+          //  const token =  jwt.sign(
+          //      {user_momo_number: existing_user.mobile_money_number, user_NIN_number: existing_user.NIN_number  },
+          //      jwtSecret,
+          //      {
+          //      expiresIn: maxAge, // 3hrs in sec
+          //      }
+          //  );
+
+          //  console.log(token)
+
+          //  // res.send({"msg":"User exist"});
+
+          //  res.cookie("jwt", token, {
+          //      httpOnly: true,
+          //      maxAge: maxAge * 1000, // 3hrs in ms
+          //  });
+
+          //end
+
         })
     
       }
