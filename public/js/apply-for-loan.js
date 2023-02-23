dc5d88ae36b1324a7286e0813bd88faa68f56210
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-02-23 05:09:48 +0000

    Display amount to be pay back in real time on loan application page

--- public/js/apply-for-loan.js
+++ public/js/apply-for-loan.js
@@ -1,1 +1,82 @@
-alert("Working");
+let loan_amount = document.getElementById("loan-amount");
+let payment_date = document.getElementById("payment-date");
+let estimated_payment_result = document.getElementById("estimated-payment-result");
+let interest_rate = 0.0033;
+console.log(loan_amount, payment_date);
+
+let loan_amount_value = 0;
+let payment_date_value = 0;
+
+
+// Get current date and track the day the person take the loan
+const date = new Date();
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let current_date = `${month}/${day}/${year}`;
+
+console.log(current_date);
+
+
+
+
+
+
+
+loan_amount.addEventListener("input", ()=>{
+    loan_amount_value = loan_amount.value;
+
+    loan_amount_value = parseInt(loan_amount_value);
+    console.log(typeof loan_amount_value)
+    console.log(loan_amount_value)
+})
+
+
+
+payment_date.addEventListener("input", estimatePayment)
+
+
+function estimatePayment(){
+
+    if(!loan_amount_value){
+
+            payment_date_value = payment_date.value;
+            // alert(payment_date_value);
+            
+            let days = getNumberOfDays(current_date, payment_date_value);
+            days = Math.abs(days)
+            console.log(days);
+
+            let sub_calculation = 1+interest_rate*days ;
+            let amount = loan_amount_value * sub_calculation;
+
+            console.log(sub_calculation);
+            console.log(amount);
+
+            estimated_payment_result.innerHTML = `$${amount} LRD`;
+            
+    }
+
+    alert("Enter the amount.");
+    window.location.reload();
+}
+
+
+// implement calculation of days the person have to pay back 
+function getNumberOfDays(start, end) {
+    const date1 = new Date(start);
+    const date2 = new Date(end);
+
+    // One day in milliseconds
+    const oneDay = 1000 * 60 * 60 * 24;
+
+    // Calculating the time difference between two dates
+    const diffInTime = date2.getTime() - date1.getTime();
+
+    // Calculating the no. of days between two dates
+    const diffInDays = Math.round(diffInTime / oneDay);
+
+    return diffInDays;
+}
+
+// alert("Working");
