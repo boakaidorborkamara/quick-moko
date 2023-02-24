a29e88ab6d3765d69c25fb43dd08b548f5067906
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-02-24 00:14:13 +0000

    Refactor code in apply for loan js file for readibility purpose

--- public/js/apply-for-loan.js
+++ public/js/apply-for-loan.js
@@ -1,72 +1,92 @@
-const { response } = require("express");
-
+// Get HTML elements 
 let loan_amount = document.getElementById("loan-amount");
 let payment_date = document.getElementById("payment-date");
 let estimated_payment_result = document.getElementById("estimated-payment-result");
 let apply_for_loan_btn = document.getElementById("apply-for-loan-btn");
 let result_area = document.getElementById("result-area");
+
+
 // hide result area 
 result_area.style.display = "none";
 
 
-
+// Iniatialize interest rate
 let interest_rate = 0.0033;
-console.log(loan_amount, payment_date);
 
+
+//Create variables that store actual value user request as loan amount and payment date
 let loan_amount_value = 0;
 let payment_date_value = 0;
 
 
+//Create a variable that stores amount to be pay back after doing interest calculations
+let payback_amount = 0;
+
+
 // Get current date and track the day the person take the loan
-const date = new Date();
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
-let current_date = `${month}/${day}/${year}`;
-
-console.log(current_date);
+let loan_request_date = getDateUserRequestForAloan();
 
 
-
-
-
-
-
+//Upadate loan_amount_value variable with latest values as user interacts with input field
 loan_amount.addEventListener("input", ()=>{
+
+    // update variable with latest value 
     loan_amount_value = loan_amount.value;
 
+    // change datatype of variable to number
     loan_amount_value = parseInt(loan_amount_value);
-    console.log(typeof loan_amount_value)
-    console.log(loan_amount_value)
+
+});
+
+
+// update payment_date_value variable as user interacts with input field 
+payment_date.addEventListener("input", ()=>{
+
+    payment_date_value = payment_date.value;
+    estimatePayBackAmount()
+
+});
+
+
+apply_for_loan_btn.addEventListener('click', ()=>{
+
+    // display error messages 
+    // result_area.innerHTML = "We are working on intergrating MoMo to receive your loans";
+    result_area.innerHTML = `Requested loan amount: ${loan_amount_value}`;
+
+        
+    result_area.style.display = "block";
+        
+    setTimeout(() => {
+        result_area.style.display = "none";
+        window.location.reload();
+    }, 7000);
+      
 })
 
 
 
-payment_date.addEventListener("input", estimatePayment)
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// amount to be pay back 
-let amount = 0;
 
-
-function estimatePayment(){
+// Carry out an interest formula and come up with amount a user should pay base on the amount they are taking
+function estimatePayBackAmount(){
 
     if(loan_amount_value){
-
-            payment_date_value = payment_date.value;
-            // alert(payment_date_value);
             
-            let days = getNumberOfDays(current_date, payment_date_value);
-            days = Math.abs(days)
-            console.log(days);
+            let days = getNumberOfDaysBeforeLoanIsPayback(loan_request_date, payment_date_value);
+            days = Math.abs(days); //change negative number to positive
 
+
+            // Implement interst formula to get amount user needs to pay back 
             let sub_calculation = 1+interest_rate*days ;
-            amount = loan_amount_value * sub_calculation;
+            payback_amount = loan_amount_value * sub_calculation;
 
             console.log(sub_calculation);
-            console.log(amount);
+            console.log(payment_date);
 
-            estimated_payment_result.innerHTML = `$${amount} LRD`;
+            estimated_payment_result.innerHTML = `$${payback_amount} LRD`;
 
             return;
             
@@ -78,7 +98,7 @@ function estimatePayment(){
 
 
 // implement calculation of days the person have to pay back 
-function getNumberOfDays(start, end) {
+function getNumberOfDaysBeforeLoanIsPayback(start, end) {
     const date1 = new Date(start);
     const date2 = new Date(end);
 
@@ -95,25 +115,18 @@ function getNumberOfDays(start, end) {
 }
 
 
+// Keep track of the date the user request for a loan 
+function getDateUserRequestForAloan(){
 
-apply_for_loan_btn.addEventListener('click', ()=>{
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let current_date = `${month}/${day}/${year}`;
 
-    // display error messages 
-    // result_area.innerHTML = "We are working on intergrating MoMo to receive your loans";
-    result_area.innerHTML = `Requested loan amount: ${loan_amount_value}`;
+    return current_date;
 
-        
-    result_area.style.display = "block";
-        
-    setTimeout(() => {
-        result_area.style.display = "none";
-        window.location.reload();
-    }, 7000);
-    
-
-   
-    
-})
+}
 
 
 // Implement payment for requested amount 
