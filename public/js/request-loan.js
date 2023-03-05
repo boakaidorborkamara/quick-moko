f343818fd7724969c6d2295d5d19fd554fc44e12
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-03-05 02:08:11 +0000

    Cojnnect js fileto loan requestmodal on loan transaction page

--- public/js/request-loan.js
+++ public/js/request-loan.js
@@ -5,6 +5,7 @@ let estimated_payment_result = document.getElementById("estimated-payment-result
 let apply_for_loan_btn = document.getElementById("apply-for-loan-btn");
 let result_area = document.getElementById("result-area");
 
+alert("Working")
 
 // hide result area 
 result_area.style.display = "none";
